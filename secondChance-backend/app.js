ebb241df7a7dc0c402d0cdae3b1f740789a16a97
viktoria-yarema <yarema1815@gmail.com>
2024-11-29 19:17:28 +0100

    Implement Search service API

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -25,10 +25,8 @@ app.use(express.json());
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
 
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const searchRoutes = require("./routes/searchRoutes")
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -39,11 +37,8 @@ app.use(pinoHttp({ logger }));
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
 
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 // Global Error Handler
