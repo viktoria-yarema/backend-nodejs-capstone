13af7e6ae07d59b402686aaa0c98ad28adcabc0f
viktoria-yarema <yarema1815@gmail.com>
2024-12-01 13:02:01 +0100

    Implement User Profile backend service API

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -85,4 +85,52 @@ router.post('/login', async (req, res) => {
     }
 });
 
+const { body, validationResult } = require('express-validator');
+
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      logger.error('Validation errors in update request', errors.array());
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    try {
+    const email = req.headers.email;
+
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({ error: "Email not found in the request headers" });
+    }
+
+    const db = await connectToDatabase();
+
+    const collection = db.collection("users");
+
+    const existingUser = await collection.findOne({ email });
+
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: 'after' }
+    );
+
+    const payload = {
+        user: {
+           id: updatedUser._id.toString(),
+        },
+    };
+    
+    const authtoken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+    logger.info('User updated successfully');
+
+    return res.status(200).json({authtoken});
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
